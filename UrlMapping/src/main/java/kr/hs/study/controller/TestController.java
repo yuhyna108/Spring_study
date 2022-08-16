f297856c7c82b9c3588efc7ff893c05a4c258b9e
yuhyna108 <81597393+yuhyna108@users.noreply.github.com>
2022-08-16 11:52:10 +0900

    UrlMapping Project upload

---
+++ UrlMapping/src/main/java/kr/hs/study/controller/TestController.java
@@ -0,0 +1,32 @@
+package kr.hs.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("sub2") /*공통되는 부분 표시*/
+public class TestController {
+	/*
+	 * @GetMapping("/sub2/test5") public String test5() { return "result"; }
+	 * 
+	 * @GetMapping("/sub2/test6") public String test6() { return "result"; }
+	 * 
+	 * @GetMapping("/sub2/test7") public String test7() { return "result"; }
+	 */
+	
+	@GetMapping("/test5")
+	public String test5() {
+		return "result";
+	}
+	
+	@GetMapping("/test6")
+	public String test6() {
+		return "result";
+	}
+	
+	@GetMapping("/test7")
+	public String test7() {
+		return "result";
+	}
+}
---
+++ UrlMapping/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>hi spring MVC~</h1>
+<img src="img/1.svg" width="150" height="50"/>
+<a href="result?a=10&b=20&c=30&c=40&c=50">result</a><br/>
+</body>
+</html>
